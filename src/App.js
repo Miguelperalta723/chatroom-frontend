5cd61d0ca1cb314f5e1673fdaf31ed9d58ca8971
Miguel Peralta <eudysperalta723@gmail.com>
2020-02-18 11:58:52 -0800

    home component

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ import ChatForm from './components/chatForm.js'
 import FormikLoginForm from './components/LoginForm.js'
 import RegisterForm from './components/RegisterForm.js'
 import  Messages  from './components/messages.js'
+import Home from './components/Home.js'
 
 function App() {
   return (
     <div className="App">
+      <Route exact path="/" component={Home} />
      <PrivateRoute path="/chat" component={Messages} />
      <Route path="/login" component={FormikLoginForm} />
      <Route path="/register" component={RegisterForm} />
