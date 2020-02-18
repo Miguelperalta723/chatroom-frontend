cb4a87fc6cba4d56277b904385a21e2b8adb3ab6
Miguel Peralta <eudysperalta723@gmail.com>
2020-02-18 12:05:42 -0800

    add login link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+
 
 const Home = () => {
     return(
         <div>
             <h3>Home</h3>
+            <Link to="/login">login</Link>
         </div>
     )
 }
